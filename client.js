9f67bc3886629e545822e96fbee489fdba258747
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-13 18:11:06 -0600

    echo server done

--- client.js
+++ client.js
@@ -1,8 +1,19 @@
 const {Socket} = require("net");
+const readline = require("readline").createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
 const socket = new Socket();
+socket.setEncoding("utf-8");
 
 socket.connect({
 	host: "localhost",
 	port: 8000,
 });
+
+readline.on("line", (line) => {
+	socket.write(line);
+});
+
+socket.on("data", console.log);
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const {Server} = require("net");
 
 const server = new Server();
 
-server.listen({port: 8000, host: "0.0.0.0"}, () => {
-	console.log("Listening on port 8000");
+server.on("connection", (socket) => {
+	console.log(`New connection from ${socket.remoteAddress}:${socket.remotePort}`);
+	socket.setEncoding("utf-8");
+	socket.on("data", (data) => {
+		console.log(data);
+		socket.write(data);
+	});
 });
+
+server.listen({port: 8000, host: "0.0.0.0"}, () => console.log("Listening on port 8000"));
